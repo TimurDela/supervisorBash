b5709100a72fb0c47a94deac448fb975a7a866ea
TimurDela <32736336+TimurDela@users.noreply.github.com>
2021-02-16 17:27:11 +0100

    Update eventlistener.sh
    correct expression for line break

--- eventlistener.sh
+++ eventlistener.sh
@@ -13,11 +13,11 @@ do
 done
 >&2 echo "Incoming REMOTE_COMMUNICATION: ${item:$sep} characters will be read" # Print PAYLOAD_LENGTH to stderr
 read -N ${item:$sep} -r msg # read the entire event message (including line break)
-sep=$((`expr index "$msg" $"\r"`)) # find line break index
+sep=$((`expr index "$msg" $'\n'`)) # find line break index
 if (( $sep > 0 ));
 then
-    type=${msg:5:$sep-5} #store event type in $type
-    data=${msg:$sep+1} #store event data in $data
+    type=${msg:5:$sep-6} #store event type in $type
+    data=${msg:$sep} #store event data in $data
     >&2 echo "$type -- data:$data" # print message content to stderr
     echo "RESULT 2" 
     echo "OK" # do not use \n here even with $''
